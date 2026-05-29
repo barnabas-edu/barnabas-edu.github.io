00a690523bd21f96b50064001bedc50a83ef59fc
Horváth Barnabás <barnabas@teachers.org>
2026-05-29 14:12:54 +0200

    Update headerFooterManager.js

--- js/headerFooterManager.js
+++ js/headerFooterManager.js
@@ -3,33 +3,33 @@ class MyHeader extends HTMLElement {
 		this.innerHTML = `
 		<header class="header" id="header">
         <div class="navbar">
-                <div><a href="https://barnabas-edu.github.io/tanitas/" class="nav__logo">Horváth Barnabás Oldala</a></div>
+                <div><a href="https://barnabas-edu.github.io" class="nav__logo">Horváth Barnabás Oldala</a></div>
                 <div class="nav__menu" id="nav-menu">
                     <ul class="nav__list">
 
                         <li class="nav__item">
-                            <a href="/tanitas/" class="nav__link">
+                            <a href="/" class="nav__link">
                                 <i class='bx bx-home-alt nav__icon'></i>
                                 <span class="nav__name">Kezdőlap</span>
                             </a>
                         </li>
 
                         <li class="nav__item">
-                            <a href="/tanitas/tananyagok/" class="nav__link">
+                            <a href="/tananyagok/" class="nav__link">
                                 <i class='bx bx-book nav__icon'></i>
                                 <span class="nav__name">Tananyagok</span>
                             </a>
                         </li>
 
                         <li class="nav__item">
-                            <a href="/tanitas/szamologep/" class="nav__link">
+                            <a href="/szamologep/" class="nav__link">
                                 <i class='bx bx-calculator nav__icon'></i>
                                 <span class="nav__name">Számológép</span>
                             </a>
                         </li>
 						
 						<li class="nav__item">
-                            <a href="/tanitas/rolam/" class="nav__link">
+                            <a href="/rolam/" class="nav__link">
                                 <i class='bx bx-user nav__icon'></i>
                                 <span class="nav__name">Rólam</span>
                             </a>
