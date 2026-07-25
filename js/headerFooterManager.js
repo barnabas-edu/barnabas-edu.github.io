b6f8449b20031b8622b06c589c17b6f8b2358c1c
Horváth Barnabás <barnabas@teachers.org>
2026-07-25 10:35:00 +0200

    Update headerFooterManager.js

--- js/headerFooterManager.js
+++ js/headerFooterManager.js
@@ -47,7 +47,8 @@ class MyHeader extends HTMLElement {
             <div class="section-title" style="text-align: center">Mód</div>
             <div class="option-group" id="modeOptions">
 				<div class="option" data-mode="auto"><span class="material-symbols-outlined">routine</span> Auto</div>
-                <div class="option" data-mode="light"><span class="material-symbols-outlined"><use href="icon/light_mode.svg"/></span> Világos</div>
+                <div class="option" data-mode="light">
+				<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M565-395q35-35 35-85t-35-85q-35-35-85-35t-85 35q-35 35-35 85t35 85q35 35 85 35t85-35Zm-226.5 56.5Q280-397 280-480t58.5-141.5Q397-680 480-680t141.5 58.5Q680-563 680-480t-58.5 141.5Q563-280 480-280t-141.5-58.5ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/></svg>Világos</div>
                 <div class="option" data-mode="dark"><span class="material-symbols-outlined">dark_mode</span> Sötét</div>
             </div>
 
